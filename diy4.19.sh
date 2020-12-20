55d11f739e5298947b528928714d755a62e99e22
firkerword <61251923+firkerword@users.noreply.github.com>
2020-12-20 09:50:55 +0800

    Update diy4.19.sh

--- diy4.19.sh
+++ diy4.19.sh
@@ -29,8 +29,7 @@ git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-ap
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 git clone https://github.com/jerrykuku/luci-app-ttnode.git package/luci-app-ttnode
-git clone https://github.com/lede/luci-app-ledeproxy.git package/luci-app-ledeproxy
-# svn co https://github.com/firker/diy-ziyong/trunk/smartdns package/smartdns
+git clone https://github.com/ledewrt/luci-app-ledeproxy.git package/luci-app-ledeproxy
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
