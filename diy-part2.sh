37f3b073340e6b08f8f384b417b3b6af3673d78f
firkerword <61251923+firkerword@users.noreply.github.com>
2021-12-22 19:34:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,4 +21,5 @@ rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lean/luci-app-serverchan
 rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/https-dns-proxy
+rm -rf ./feeds/diy/openwrt-adguardhome
 svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
