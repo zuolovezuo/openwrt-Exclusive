18efb31b5549d32f0b006c1ea491b8d44d050b5a
firkerword <61251923+firkerword@users.noreply.github.com>
2022-03-17 11:05:42 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -15,13 +15,13 @@ sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-SE'/g" ./package/base-files/files
 # curl -fsSL  https://raw.githubusercontent.com/siropboy/other/master/patch/poweroff/system.lua > ./feeds/luci/modules/luci-mod-admin-full/luasrc/controller/admin/system.lua
 # curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/logo.jpg > .package/luci-app-serverchan/root/usr/bin/serverchan/api/logo.jpg
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 rm -rf ./package/lean/luci-app-qbittorrent
 rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lean/luci-app-serverchan
 rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/smartdns
 # rm -rf ./feeds/packages/net/https-dns-proxy
-rm -rf ./feeds/diy/adguardhome
+# rm -rf ./feeds/diy/adguardhome
 # rm -rf ./feeds/diy/luci-app-adguardhome
 # svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
