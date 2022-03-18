73a639e093f19baae0b43afc06e7f3d7d10c442c
firkerword <61251923+firkerword@users.noreply.github.com>
2022-03-18 22:54:29 +0800

    Update diy5.4.sh

--- diy5.4.sh
+++ diy5.4.sh
@@ -17,8 +17,8 @@ sed -i 's/KERNEL_TESTING_PATCHVER:=5.10/KERNEL_TESTING_PATCHVER:=5.4/g' ./target
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 
-git clone https://github.com/jerrykuku/openwrt-package.git package/vssr
 git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
