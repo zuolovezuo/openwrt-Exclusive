85e567ac92c4e7a60495c0d93bf36433b82a32fb
firkerword <61251923+firkerword@users.noreply.github.com>
2022-05-14 12:56:33 +0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -35,8 +35,10 @@ git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
 svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 sed -i 's/luci-lib-ipkg/luci-base/g' package/luci-app-store/Makefile
 svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
-
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
+
+svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-cpulimit package/luci-app-cpulimit
+svn co https://github.com/immortalwrt/packages/trunk/utils/cpulimit package/cpulimit
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 # svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
