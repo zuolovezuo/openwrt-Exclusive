693cc8ba3622f12de7952f42e6deb78a7d965446
firkerword <61251923+firkerword@users.noreply.github.com>
2022-06-09 21:11:20 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -18,9 +18,8 @@ curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yam
 # wget https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml -O ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-rm -rf ./package/lean/luci-app-qbittorrent
-rm -rf ./package/lean/luci-theme-argon
-rm -rf ./package/lean/luci-app-serverchan
+# rm -rf ./feeds/luci/applications/luci-app-qbittorrent
+rm -rf ./feeds/luci/applications/luci-app-serverchan
 # rm -rf ./package/diy-ziyong/adguardhome
 rm -rf ./feeds/packages/net/adguardhome
 rm -rf ./feeds/packages/net/smartdns
