313087734e653f0759d280f975bcf31b543853af
firkerword <61251923+firkerword@users.noreply.github.com>
2022-10-15 15:06:12 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -29,3 +29,5 @@ rm -rf ./package/openwrt-passwall/v2ray-geodata
 rm -rf ./package/ssr/v2ray-geodata
 # rm -rf ./feeds/packages/net/https-dns-proxy
 # svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+rm -rf feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
