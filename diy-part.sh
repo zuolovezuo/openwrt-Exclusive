ce847541b4ae591cc95811ea1788f39bb8f476f5
firkerword <61251923+firkerword@users.noreply.github.com>
2022-10-27 23:10:48 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -27,7 +27,11 @@ rm -rf ./feeds/packages/net/mosdns
 rm -rf ./feeds/luci/luci-theme-argon
 rm -rf ./package/openwrt-passwall/v2ray-geodata
 rm -rf ./package/ssr/v2ray-geodata
+rm -rf .package/sirpdboy-package/smartdns
+rm -rf .package/sirpdboy-package/adguardhome
+rm -rf .package/sirpdboy-package/luci-app-dockerman
+rm -rf .package/sirpdboy-package/luci-app-autotimeset
 # rm -rf ./feeds/packages/net/https-dns-proxy
 # svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
-rm -rf feeds/packages/lang/golang
-svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
+# rm -rf feeds/packages/lang/golang
+# svn export https://github.com/sbwml/packages_lang_golang/trunk feeds/packages/lang/golang
