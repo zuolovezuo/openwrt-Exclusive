f6593cd350bce2ce67579546ca3ad22032b1678d
firkerword <61251923+firkerword@users.noreply.github.com>
2022-11-03 23:39:33 +0800

    Update ipk.sh

--- ipk.sh
+++ ipk.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
+# Add a feed source
+
 sed -i '$a src-git ipks https://github.com/QiuSimons/openwrt-mos.git' feeds.conf.default
 
