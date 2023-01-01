2ba8e1720bb482747341419a7a5cee8a52ca80a8
firkerword <61251923+firkerword@users.noreply.github.com>
2023-01-01 16:16:18 +0000

    Update diy6.0.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -26,7 +26,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lu
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-git clone https://github.com/firkerword/luci-app-lucky-g.git package/lucky
+git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 # git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 # git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
