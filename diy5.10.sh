a362e1a7bd484e80d7a6d3c55e9dac7f8c977445
firkerword <61251923+firkerword@users.noreply.github.com>
2023-02-01 18:32:17 +0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -9,7 +9,7 @@
 #=============================================================
 
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
-sed -i 's/KERNEL_PATCHVER:=6.2/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 
 # Uncomment a feed source
