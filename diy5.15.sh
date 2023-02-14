a4879543c6f220cbe57ac23653d5aab05431966f
firkerword <61251923+firkerword@users.noreply.github.com>
2023-02-14 08:45:41 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -22,7 +22,7 @@ git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luc
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
 git clone https://github.com/gngpp/luci-theme-neobird.git package/luci-theme-neobird
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
- find ./ | grep Makefile | grep mosdns | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 # git clone https://github.com/firkerword/openwrt-mos.git package/openwrt-mos
 git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
