52905aee953d6d9dd220f3ead38e9bb16da06160
firkerword <61251923+firkerword@users.noreply.github.com>
2023-03-07 20:02:24 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -23,14 +23,14 @@ git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luc
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
 git clone https://github.com/gngpp/luci-theme-neobird.git package/luci-theme-neobird
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 # git clone https://github.com/firkerword/openwrt-mos.git package/openwrt-mos
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 # git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone -b v5-dev https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
 git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/firkerword/smartdns.git package/smartdns
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
