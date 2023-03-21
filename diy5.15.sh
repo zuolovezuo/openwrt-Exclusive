4c410fadd2a3afa884e4b671b80a954e49188938
firkerword <61251923+firkerword@users.noreply.github.com>
2023-03-22 00:28:47 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -28,11 +28,11 @@ find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-# git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
-git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/firkerword/smartdns.git package/smartdns
+# git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
+# git clone https://github.com/firkerword/smartdns.git package/smartdns
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
