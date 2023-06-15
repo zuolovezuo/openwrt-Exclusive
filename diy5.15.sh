e73752268cf7ce45ca81455008aaafcccc8b2834
firkerword <61251923+firkerword@users.noreply.github.com>
2023-06-16 00:13:13 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -38,7 +38,7 @@ find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
 git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 # git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
