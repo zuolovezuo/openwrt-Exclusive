8d90931c727ed8330c1e80dd07671caf21d3803d
firkerword <61251923+firkerword@users.noreply.github.com>
2023-09-14 10:32:48 +0800

    Update diy6.0.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -31,7 +31,6 @@ git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 # git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
