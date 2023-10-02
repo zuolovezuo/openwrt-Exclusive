37c49f6ce52f081d76b21e2e3eb8a42da77facea
firkerword <61251923+firkerword@users.noreply.github.com>
2023-10-02 14:07:49 +0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -7,8 +7,8 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=============================================================
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
-sed -i 's/KERNEL_PATCHVER:=6.2/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=6.2/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
