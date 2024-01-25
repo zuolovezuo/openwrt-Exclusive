a9bd12bb5620a1eec2a37634323232f41672ede3
firkerword <61251923+firkerword@users.noreply.github.com>
2024-01-26 01:14:30 +0800

    Update diy6.6.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -8,7 +8,7 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 # sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.2/g' ./target/linux/x86/Makefile
 
 
