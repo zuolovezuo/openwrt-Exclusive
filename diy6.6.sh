98ffe2e91715a6593f30ecd5ee1c0827bbc9cb0e
firkerword <61251923+firkerword@users.noreply.github.com>
2024-03-03 01:14:58 +0800

    Create diy6.6.sh

---
+++ diy6.6.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
+
+function merge_package(){
+    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
+    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
+    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
+    git clone --depth=1 --single-branch $1
+    mv $2 package/custom/
+    rm -rf $repo
+}
+function drop_package(){
+    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
+}
+
+rm -rf package/custom; mkdir package/custom
+
+# Add a feed source
+git clone https://github.com/fw876/helloworld.git package/ssr
+git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
+git clone https://github.com/gngpp/luci-theme-neobird.git package/luci-theme-neobird
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+# find ./ | grep Makefile | grep mosdns | xargs rm -f
+# git clone https://github.com/firkerword/openwrt-mos.git package/openwrt-mos
+# git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
+# git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
