b74bdba414cf68b70928e67e38aad585413f2651
firkerword <61251923+firkerword@users.noreply.github.com>
2024-08-12 23:35:03 +0800

    Update diy6.0.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -63,6 +63,6 @@ merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclas
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 git clone https://github.com/sbwml/luci-app-dae package/dae
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
