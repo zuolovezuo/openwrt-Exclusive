1f7849d673859d0808e52613b4c13d04fd254f3b
firkerword <61251923+firkerword@users.noreply.github.com>
2024-11-24 09:20:29 +0800

    Update diy2305.sh

--- diy2305.sh
+++ diy2305.sh
@@ -10,7 +10,7 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 # sed -i '/luci/d' feeds.conf.default
 # echo 'src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05' >>feeds.conf.default
