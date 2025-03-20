696783053eb5e489b044488ed7e6a0d15b32acd1
firker <56651759+firker@users.noreply.github.com>
2025-03-20 16:18:03 +0800

    Update diy2305.sh

--- diy2305.sh
+++ diy2305.sh
@@ -41,6 +41,7 @@ git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git  package/luci-t
 # git clone https://github.com/firkerword/openwrt-mos.git package/openwrt-mos
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/openwrt-nikki
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
